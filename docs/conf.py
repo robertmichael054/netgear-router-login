b097cdad830ce733c977e1f18118f76c9035569b
robertmichael054 <robertmichael054@outlook.com>
2026-01-22 14:43:44 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+project = 'netgear router login'
+author = 'netgear router login'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']   
+
+html_js_files = [
+    'chatbot.js',
+]
+html_favicon = '_static/favicon.png'
